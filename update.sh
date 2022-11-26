c5fe0508050d2c0cb6c9a18de0218a596ccb6266
Oli Allen <oli@oliallen.com>
2022-11-26 20:34:30 +0000

    Added git pull script

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OLD_HEAD=$(git rev-parse HEAD)
+git pull
+NEW_HEAD=$(git rev-parse HEAD)
+[ $OLD_HEAD = $NEW_HEAD ] && exit 0 # no change to repo = no change to any files
+
+
+echo "Changed"
+
