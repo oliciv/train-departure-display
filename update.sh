44f8c75e17ed2d2478f6f2925fe4053cc5b845bf
Oli Allen <oli@oliallen.com>
2023-08-02 22:59:22 +0100

    Discard local changes on update

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 OLD_HEAD=$(git rev-parse HEAD)
-git pull
+git fetch --all
+git reset --hard origin/$CURRENT_BRANCH
 NEW_HEAD=$(git rev-parse HEAD)
 [ $OLD_HEAD = $NEW_HEAD ] && exit 0 # no change to repo = no change to any files
 
